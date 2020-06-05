75fa42037e22ba87a6e9ddefbb9a08f18417ce43
lrun <lrun123>
2020-06-05 17:38:52 +0800

    初始化版本

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,35 @@
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const path = require("path");
 // vue.config.js
 module.exports = {
+  productionSourceMap: false,
+  configureWebpack: {
+    resolve: {
+      // 配置解析别名
+      extensions: [".js", ".vue", ".json"],
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        api: path.resolve(__dirname, "../src/api")
+      }
+    },
+    optimization: {
+      splitChunks: {
+        chunks: "all"
+      },
+      minimizer: [
+        new UglifyJsPlugin({
+          uglifyOptions: {
+            compress: {
+              warnings: false,
+              drop_console: true, // console
+              drop_debugger: false,
+              pure_funcs: ["console.log"] // 移除console
+            }
+          }
+        })
+      ]
+    }
+  },
   chainWebpack: config => {
     const oneOfsMap = config.module.rule("scss").oneOfs.store;
     oneOfsMap.forEach(item => {
@@ -16,5 +46,14 @@ module.exports = {
       .when(process.env.NODE_ENV === "development", config =>
         config.devtool("cheap-source-map")
       );
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .loader("vue-loader")
+      .tap(options => {
+        // modify the options...
+        options.compilerOptions.preserveWhitespace = false;
+        return options;
+      });
   }
 };
